0658c65fc63502bbb270ff064899b3790a843d11
cazacuirina <98032410+cazacuirina@users.noreply.github.com>
2023-02-22 13:35:08 +0200

    Delete proiectWeb directory

--- proiectWeb/routes/feedback.js
+++
@@ -1,9 +0,0 @@
-const controllerFeedback = require("../controllers/feedback");
-const router = require('express').Router();
-
-router.get("/", controllerFeedback.getAllFeedback);
-router.get("/count", controllerFeedback.getFeedbackCountTip);
-router.get("/:tip", controllerFeedback.getFeedbackTip);
-router.post("/", controllerFeedback.createFeedback);
-
-module.exports = router;
